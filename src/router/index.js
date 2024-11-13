ac2c5a4d50a884a2b06bc220764425853e6e495c
Jeongseok Yoon <jeongseok.yoon@outlook.com>
2024-11-13 17:23:46 +0900

    feat : 시작 화면 구현

--- src/router/index.js
+++ src/router/index.js
@@ -10,65 +10,64 @@ import LoadingScreen from '@/components/LoadingScreen.vue';
 import FailScreen from '@/components/FailScreen.vue';
 import SuccessScreen from '@/components/SuccessScreen.vue';
 import ChallengePlayView from '@/views/ChallengePlayView.vue';
+import StartView from '@/views/StartView.vue';
 
 const routes = [
   {
     // path: '/',
     path: '/home-view',
+  {
+    path: '/',
+    name: 'StartView',
+    component: StartView,
+  },
+  {
+    path: '/home-view',                              
     name: 'HomeView',
     component: HomeView,
   },
   {
-    // path: '/',
     path: '/loading-screen',
     name: 'LoadingScreen',
     component: LoadingScreen,
   },
   {
-    // path: '/',
     path: '/challenge-select-view',
     name: 'ChallengeSelectView',
     component: ChallengeSelectView,
   },
   {
-    // path: '/',
     path: '/fail-screen',
     name: 'FailScreen',
     component: FailScreen,
   },
   {
-    // path: '/',
     path: '/success-screen',
     name: 'SuccessScreen',
     component: SuccessScreen,
   },
   {
-    // path: '/',
     path: '/game-select-view',
     name: 'GameSelectView',
     component: GameSelectView,
   },
   {
-    // path: '/',
     path: '/login-view',
     name: 'LoginView',
     component: LoginView,
   },
   {
-    // path: '/',
     path: '/sign-up-view',
     name: 'SignUpView',
     component: SignUpView,
   },
   {
-    // path: '/',
     path: '/game-play-view',
     name: 'GamePlayView',
     component: GamePlayView,
   },
   {
-    path: '/',
-    // path: '/challenge-play-view',
+    path: '/challenge-play-view',
     name: 'ChallengePlayView',
     component: ChallengePlayView,
   },
