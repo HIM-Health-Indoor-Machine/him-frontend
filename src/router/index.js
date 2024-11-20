fc3dd6115e627073d6262074a3df25f28f5d74c3
Jeongseok Yoon <jeongseok.yoon@outlook.com>
2024-11-21 08:57:34 +0900

    feat: 테마 api 연동

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import StartView from '@/views/StartView.vue';
 import LoginView from '@/views/LoginView.vue';
 import SignUpView from '@/views/SignUpView.vue';
 import HomeView from '@/views/HomeView.vue';
-import ChallengeSelectView from '@/views/ChallengeSelectView.vue';
 import GameSelectView from '@/views/GameSelectView.vue';
-import FailScreen from '@/components/FailScreen.vue';
-import SuccessScreen from '@/components/SuccessScreen.vue';
-import ChallengePlayView from '@/views/ChallengePlayView.vue';
-import StartView from '@/views/StartView.vue';
 import GamePlayViewDuck from '@/views/GamePlayViewDuck.vue';
 import GamePlayViewBomb from '@/views/GamePlayViewBomb.vue';
 import GamePlayViewFlying from '@/views/GamePlayViewFlying.vue';
+import ChallengeSelectView from '@/views/ChallengeSelectView.vue';
+import ChallengePlayView from '@/views/ChallengePlayView.vue';
+import SuccessScreen from '@/components/SuccessScreen.vue';
+import FailScreen from '@/components/FailScreen.vue';
 
 const routes = [
   {
     path: '/',
     name: 'StartView',
     component: StartView,
-  },
-  {
-    path: '/home-view',                              
-    name: 'HomeView',
-    component: HomeView,
   },
   {
     path: '/login-view',
@@ -34,22 +29,27 @@ const routes = [
     component: SignUpView,
   },
   {
+    path: '/home-view',                              
+    name: 'HomeView',
+    component: HomeView,
+  },
+  {
     path: '/game-select-view/:userId',
     name: 'GameSelectView',
     component: GameSelectView,
   },
   {
-    path: '/game-play-view-flying',
+    path: '/game-play-view-flying/:id/:type/:difficultyLevel/:theme/:userId',
     name: 'GamePlayViewFlying',
     component: GamePlayViewFlying,
   },
   {
-    path: '/game-play-view-bomb',
+    path: '/game-play-view-bomb/:id/:type/:difficultyLevel/:theme/:userId',
     name: 'GamePlayViewBomb',
     component: GamePlayViewBomb,
   },
   {
-    path: '/game-play-view-duck',
+    path: '/game-play-view-duck/:id/:type/:difficultyLevel/:theme/:userId',
     name: 'GamePlayViewDuck',
     component: GamePlayViewDuck,
   },
@@ -59,17 +59,17 @@ const routes = [
     component: ChallengeSelectView,
   },
   {
-    path: '/challenge-play-view/:challengeId',
+    path: '/challenge-play-view/:challengeId/:userId',
     name: 'ChallengePlayView',
     component: ChallengePlayView,
   },
   {
-    path: '/fail-screen',
+    path: '/fail-screen/:id/:type/:difficultyLevel/:theme/:userId',
     name: 'FailScreen',
     component: FailScreen,
   },
   {
-    path: '/success-screen',
+    path: '/success-screen/:userId/:expPoints',
     name: 'SuccessScreen',
     component: SuccessScreen,
   },
